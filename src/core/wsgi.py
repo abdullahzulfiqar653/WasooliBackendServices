b85905156816ab710a81781a1473361a9b1ba3c5
abdullahzulfiqar653 <abdullahzulfiqar653@gmail.com>
2025-01-13 17:13:47 +0500

    path in deployment added

--- src/core/wsgi.py
+++ src/core/wsgi.py
@@ -1,7 +1,8 @@
 import os
-
+import sys
 from django.core.wsgi import get_wsgi_application
 
+sys.path.append("/root/wasooli.online/dev/WasooliBackendServices/src")
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "src.settings")
 
 application = get_wsgi_application()
