e08e78ca2f48dc1138925e581402d4f94a94ac18
abdullahzulfiqar653 <abdullahzulfiqar653@gmail.com>
2025-01-19 18:44:25 +0500

    permissions command added

--- src/apis/common/contants.py
+++ src/apis/common/contants.py
@@ -9,3 +9,47 @@ MODEL_CODES = {
     "TransactionHistory": "106",
     "MerchantMembership": "107",
 }
+
+
+MERCHANT_PERMISSIONS = {
+    "transactionhistory": ["view_transactionhistory"],
+    "invoice": ["add_invoice", "change_invoice", "view_invoice"],
+    "merchantmember": [
+        "add_merchantmember",
+        "view_merchantmember",
+        "change_merchantmember",
+        "delete_merchantmember",
+    ],
+    "merchantmembership": [
+        "add_merchantmembership",
+        "change_merchantmembership",
+        "view_merchantmembership",
+        "delete_merchantmembership",
+    ],
+    "supplyrecord": [
+        "add_supplyrecord",
+        "view_supplyrecord",
+        "change_supplyrecord",
+        "delete_supplyrecord",
+    ],
+}
+
+STAFF_PERMISSIONS = {
+    "transactionhistory": ["view_transactionhistory"],
+    "invoice": ["add_invoice", "change_invoice", "view_invoice"],
+    "merchantmember": [
+        "add_merchantmember",
+        "view_merchantmember",
+        "change_merchantmember",
+    ],
+    "merchantmembership": [
+        "add_merchantmembership",
+        "change_merchantmembership",
+        "view_merchantmembership",
+    ],
+    "supplyrecord": [
+        "add_supplyrecord",
+        "view_supplyrecord",
+        "change_supplyrecord",
+    ],
+}
---
+++ src/apis/management/commands/update_permissions.py
@@ -0,0 +1,35 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth.models import Group, Permission
+from apis.common.contants import MERCHANT_PERMISSIONS, STAFF_PERMISSIONS
+
+
+class Command(BaseCommand):
+    help = "Assign or update permissions for Staff and Merchant groups"
+
+    def handle(self, *args, **kwargs):
+        staff_group, _ = Group.objects.get_or_create(name="Staff")
+        merchant_group, _ = Group.objects.get_or_create(name="Merchant")
+
+        staff_group.permissions.clear()
+        merchant_group.permissions.clear()
+
+        MERCHANT_PERMISSIONS
+
+        # Assign permissions based on the rules to Merchant
+        for model_name, allowed_perms in MERCHANT_PERMISSIONS.items():
+            permissions = Permission.objects.filter(content_type__model=model_name)
+
+            # Filter permissions based on the allowed list
+            filtered_permissions = permissions.filter(codename__in=allowed_perms)
+            merchant_group.permissions.add(*filtered_permissions)
+
+        # Assign permissions based on the rules to Staff
+        for model_name, allowed_perms in STAFF_PERMISSIONS.items():
+            permissions = Permission.objects.filter(content_type__model=model_name)
+
+            # Filter permissions based on the allowed list
+            filtered_permissions = permissions.filter(codename__in=allowed_perms)
+            staff_group.permissions.add(*filtered_permissions)
+
+        # Display the results
+        self.stdout.write(self.style.SUCCESS("Permissions successfully assigned!"))
