a709103be8f63065997b47c8ec06820e300f17b8
abdullahzulfiqar653 <abdullahzulfiqar653@gmail.com>
2025-02-14 12:45:20 +0500

    updated comments and applied select related for otp load

--- src/apis/admin/merchant.py
+++ src/apis/admin/merchant.py
@@ -34,29 +34,47 @@ class MerchantAdmin(admin.ModelAdmin):
     def get_queryset(self, request):
         # Annotate the queryset to include the count of related members
         queryset = super().get_queryset(request)
+        queryset = queryset.select_related("owner__profile__otp")
         queryset = queryset.annotate(member_count=Count("members"))
         return queryset
 
     def save_model(self, request, obj, form, change):
         """
-        Jab Merchant save hoga toh phone number bhi save karenge MerchantMember ke andar.
+        Override the save_model method to create a user, assign them to a merchant,
+        and handle related processes after saving the Merchant object.
         """
+        # Get the primary phone and email from the custom form data
         primary_phone = form.cleaned_data.get("primary_phone")
         email = form.cleaned_data.get("email", None)
 
+        # Create a new user with the primary phone as the username and email
         user = User.objects.create_user(username=primary_phone, email=email)
-        user.set_password(secrets.token_hex(7))
-        obj.owner = user  # Attach the created user to the Merchant object
-        obj.save()  # Save the Merchant object
 
+        # Set a random password for the new user
+        user.set_password(secrets.token_hex(7))
+
+        # Assign the created user to the merchant as the owner
+        obj.owner = user
+
+        # Save the Merchant object with the new user as owner
+        obj.save()
+
+        # Call the parent save_model method to handle standard save operations
         super().save_model(request, obj, form, change)
 
+        # Add the user to the "Merchant" group
         obj.owner.groups.add(Group.objects.get(name=RoleChoices.MERCHANT))
+
+        # Create a MerchantMember instance linking the user and merchant
         member = MerchantMember.objects.create(
             user=user, merchant=obj, primary_phone=primary_phone
         )
+
+        # Create an OTP record for the member and generate the OTP
         otp_record, _ = OTP.objects.get_or_create(member=member)
         otp_record.generate_otp()
+
+        # Assign the "MERCHANT" role to the newly created member
         MemberRole.objects.create(member=member, role=RoleChoices.MERCHANT)
 
     def owner_first_name(self, obj):
