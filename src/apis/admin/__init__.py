3cc44e2337c08e497b7bc0f4ad2dec36490b6a22
abdullahzulfiqar653 <abdullahzulfiqar653@gmail.com>
2025-02-23 00:18:13 +0500

    otps admin added

--- src/apis/admin/__init__.py
+++ src/apis/admin/__init__.py
@@ -1,11 +1,12 @@
+from apis.admin.otp import OTPAdmin
 from apis.admin.invoice import InvoiceAdmin
 from apis.admin.merchant import MerchantAdmin
 from apis.admin.merchant_member import MerchantMemberAdmin
 from apis.admin.transaction_history import TransactionHistoryAdmin
 from apis.admin.merchant_memberships import MerchantMembershipAdmin
 
-
 __all__ = [
+    "OTPAdmin",
     "InvoiceAdmin",
     "MerchantAdmin",
     "MerchantMemberAdmin",
