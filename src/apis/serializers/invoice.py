b19c09b41cef4ca298d2e25a8a1e306ebc7eca46
Anabiya-Fatima <anabiyafatima051@gmail.com>
2025-03-01 21:22:09 +0500

    improved logic for updating status  of paid invoice by updating transaction (#17)
    * improved logic for updating status  of paid invoice by updating transaction
    
    * improved code for storing previous state

--- src/apis/serializers/invoice.py
+++ src/apis/serializers/invoice.py
@@ -162,9 +162,20 @@ class InvoiceSerializer(serializers.ModelSerializer):
             raise serializers.ValidationError({"detail": "Invoice is already paid."})
 
         if mark_paid:
+            previous_invoice_state = [
+                {
+                    "id": instance.id,
+                    "status": instance.status,
+                    "due_amount": str(instance.due_amount),
+                }
+            ]
+
             TransactionHistory.objects.create(
                 invoice=instance,
-                metadata={"invoices": [instance.code]},
+                metadata={
+                    "invoices": [instance.code],
+                    "previous_invoice_state": previous_invoice_state,
+                },
                 merchant_membership=request.membership,
                 is_online=False,
                 credit=instance.due_amount,
