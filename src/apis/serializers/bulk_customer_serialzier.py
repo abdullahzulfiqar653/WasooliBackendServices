4ddf21e072f7121cf8cc25933eef53de32f2e83c
samisaeed626 <samisaeed626@gmail.com>
2025-03-16 04:26:08 +0500

    bulk create user endpoint done

--- src/apis/serializers/bulk_customer_serialzier.py
+++ src/apis/serializers/bulk_customer_serialzier.py
@@ -1,7 +1,6 @@
+import io
 import csv
 import secrets
-from io import TextIOWrapper
-from rest_framework import status
 from rest_framework import serializers
 from django.contrib.auth.models import User
 from apis.models.merchant_member import MerchantMember
@@ -9,79 +8,107 @@ from apis.models.member_role import MemberRole, RoleChoices
 
 from apis.models.merchant_membership import MerchantMembership
 
-
 class BulkCustomerSerializer(serializers.Serializer):
     file = serializers.FileField()
 
     def create(self, validated_data):
         request = self.context.get("request")
+        if "file" not in validated_data:
+            raise serializers.ValidationError({"file": "No file was provided"})
+
         uploaded_file = validated_data["file"]
+
         try:
-            decoded_file = TextIOWrapper(uploaded_file.file, encoding="utf-8")
+            decoded_file = io.StringIO(uploaded_file.read().decode("utf-8"))
             csv_reader = csv.DictReader(decoded_file)
 
+            users, roles, members, memberships = [], [], [], []
+            last_MerchantMembership = MerchantMembership.objects.order_by("-id").first()
+            last_account = (
+                int(last_MerchantMembership.account)
+                if last_MerchantMembership and last_MerchantMembership.account.isdigit()
+                else 0
+            )
+            last_member = MerchantMember.objects.order_by("-id").first()
+            last_code = (
+                int(last_member.code)
+                if last_member and last_member.code.isdigit()
+                else 0
+            )
             for row in csv_reader:
                 first_name = row.get("first_name", "").strip()
-                last_name = row.get("last_name", "").strip()
-                email = row.get("email", "").strip()
                 primary_phone = row.get("primary_phone", "").strip()
                 area = row.get("area", "").strip()
                 city = row.get("city", "").strip()
-                actual_price = row.get("actual_price", "0.00").strip()
-                discounted_price = row.get("discounted_price", "0.00").strip()
 
                 if not first_name or not primary_phone or not area or not city:
                     continue
-                # primary_phone should start with 3 and exactly 10 diits
-                if not primary_phone.startswith("3") or len(primary_phone) > 0:
+
+                if not primary_phone.startswith("3") or len(primary_phone) != 10:
                     continue
 
-                member = MerchantMember.objects.filter(
+                member_is = MerchantMember.objects.filter(
                     primary_phone=primary_phone
                 ).first()
-                users = []
-                roles = []
-                members = []
-                memberships = []
-                if not member:
+
+                if not member_is:
+
                     user = User(
                         first_name=first_name,
-                        last_name=last_name,
-                        email=email,
+                        email=row.get("email", "").strip(),
                         password=secrets.token_hex(7),
                         username=f"{first_name}_{secrets.token_hex(5)}",
                     )
                     users.append(user)
+                    last_code += 1
                     member = MerchantMember(
                         user=user,
+                        merchant=request.merchant,
                         primary_phone=primary_phone,
+                        code=str(last_code),
                         id=f"{MerchantMember.UID_PREFIX}{secrets.token_hex(6)}",
                     )
+
                     members.append(member)
                     role = MemberRole(
                         member=member,
                         role=RoleChoices.CUSTOMER,
                         id=f"{MemberRole.UID_PREFIX}{secrets.token_hex(6)}",
                     )
+
                     roles.append(role)
+                    last_account += 1
                     membership = MerchantMembership(
                         member=member,
                         area=area,
                         city=city,
-                        actual_price=actual_price,
-                        merchant_id=request.merchent,
-                        discounted_price=discounted_price,
+                        actual_price=row.get("actual_price", "0.00").strip(),
+                        merchant_id=request.merchant.id,
+                        discounted_price=row.get("discounted_price", "0.00").strip(),
+                        account=str(last_account),
                         id=f"{MerchantMembership.UID_PREFIX}{secrets.token_hex(6)}",
                     )
                     memberships.append(membership)
+                else:
+                    last_account += 1
+                    membership = MerchantMembership(
+                        member=member_is,
+                        area=area,
+                        city=city,
+                        actual_price=row.get("actual_price", "0.00").strip(),
+                        merchant_id=request.merchant.id,
+                        discounted_price=row.get("discounted_price", "0.00").strip(),
+                        account=str(last_account),
+                        id=f"{MerchantMembership.UID_PREFIX}{secrets.token_hex(6)}",
+                    )
+                    memberships.append(membership)
+
             User.objects.bulk_create(users)
-            MemberRole.objects.bulk_create(roles)
             MerchantMember.objects.bulk_create(members)
+            MemberRole.objects.bulk_create(roles)
             MerchantMembership.objects.bulk_create(memberships)
 
             return {"message": "Users imported successfully"}
 
         except Exception as e:
-            raise serializers.ValidationError(
-                {"error": [str(e)]}, status=status.HTTP_400_BAD_REQUEST
-            )
+            raise serializers.ValidationError({"error": [str(e)]})
--- src/apis/views/import_user.py
+++ src/apis/views/import_user.py
@@ -2,8 +2,8 @@ from apis.permissions import IsMerchantOrStaff
 from drf_spectacular.utils import extend_schema
 from rest_framework.generics import CreateAPIView
 from rest_framework.parsers import MultiPartParser, FormParser
-
-
+from rest_framework import status
+from rest_framework.response import Response
 from apis.serializers.bulk_customer_serialzier import BulkCustomerSerializer
 
 
@@ -22,3 +22,10 @@ class MerchantMemberCSVUploadView(CreateAPIView):
     permission_classes = [IsMerchantOrStaff]
     serializer_class = BulkCustomerSerializer
     parser_classes = (MultiPartParser, FormParser)
+    def post(self, request, *args, **kwargs):
+        serializer = BulkCustomerSerializer(data=request.FILES, context={"request": request})  # Use request.FILES
+        if serializer.is_valid():
+            response_data = serializer.save()
+            return Response(response_data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
