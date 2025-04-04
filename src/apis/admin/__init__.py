fb84a537733bdb004a16689e5b88979f7c827a1c
abdullahzulfiqar653 <abdullahzulfiqar653@gmail.com>
2025-04-04 18:23:56 +0500

    admin added for supply record

--- src/apis/admin/__init__.py
+++ src/apis/admin/__init__.py
@@ -1,14 +1,17 @@
 from apis.admin.otp import OTPAdmin
 from apis.admin.invoice import InvoiceAdmin
 from apis.admin.merchant import MerchantAdmin
+from apis.admin.supply_record import SupplyRecordAdmin
 from apis.admin.merchant_member import MerchantMemberAdmin
 from apis.admin.transaction_history import TransactionHistoryAdmin
 from apis.admin.merchant_memberships import MerchantMembershipAdmin
+
 
 __all__ = [
     "OTPAdmin",
     "InvoiceAdmin",
     "MerchantAdmin",
+    "SupplyRecordAdmin",
     "MerchantMemberAdmin",
     "MerchantMembershipAdmin",
     "TransactionHistoryAdmin",
